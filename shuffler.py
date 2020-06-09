14d6329cd82b9d92b69ea48120978f7387ca1fcf
SublimeMachine <66693943+SublimeMachine@users.noreply.github.com>
2020-06-09 14:09:20 -0400

    Create shuffler.py

---
+++ shuffler.py
@@ -0,0 +1,43 @@
+from collections import Counter
+import random
+
+players = 2
+packs_per_player = 6
+pack_size = 15
+pool_size = packs_per_player * pack_size
+sealed = True
+
+
+cards_in_cube = [
+    ("White", 80),
+    ("Blue", 80),
+    ("Black", 80),
+    ("Red", 80),
+    ("Green", 80),
+    ("Land", 80),
+    ("Gold", 80),
+    ("Colorless", 40),
+]
+
+colors, cards = zip(*cards_in_cube)
+if sum(cards) < pool_size * players:
+    raise Exception("Not enough cards in the cube for the specified number of players")
+
+deck = []
+[deck.extend([color] * number) for color, number in cards_in_cube]
+random.shuffle(deck)
+
+if sealed:
+    for player in range(players):
+        player_pool = deck[player * pool_size : (player + 1) * pool_size]
+        c = Counter(player_pool)
+        print(f"\nPlayer {player+1} Sealed Pool:")
+        for color in colors:
+            print(f"{color}: {c[color]}")
+else:
+    for pack in range(players * packs_per_player):
+        pack_pool = deck[pack * pack_size : (pack + 1) * pack_size]
+        c = Counter(pack_pool)
+        print(f"\nPack {pack+1} Contents:")
+        for color in colors:
+            print(f"{color}: {c[color]}")
